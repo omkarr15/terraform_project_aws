b11a3e631a5ae68d4c519aa447ab481b166a8b82
omkarr15 <130046624+omkarr15@users.noreply.github.com>
2023-10-30 16:54:19 +0530

    Update provider.tf

--- 1-basics/provider.tf
+++ 1-basics/provider.tf
@@ -3,11 +3,11 @@
 terraform {
 #tfstate maintain remotely in s3 
   backend "s3"{
-  	bucket = "tfbucketremotebackend"
-	key = "webdev/my.tfstate"
+  	bucket = "tfbucketremotebackend"   #need to create s3 bucket and give here name it will save all the .tfstate file 
+	key = "webdev/my.tfstate" #path of folder in s3 bucket
 	region = "ap-south-1"
 #state locking	in dynamodb
-	dynamodb_table = "mytableforlocktf"
+	dynamodb_table = "mytableforlocktf"      #need to create table i dynamodb in aws so it will help in state lock file
    }
   	
 	
